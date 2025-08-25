e5294107d5f556aad6aee141db5b5974071c5d81
awesomerobot <kris.aubuchon@discourse.org>
2025-08-25 16:44:54 -0400

    FIX: avoid circular reference

--- javascripts/discourse/initializers/init-force-homepage.js
+++ javascripts/discourse/initializers/init-force-homepage.js
@@ -6,8 +6,14 @@ export default {
   initialize() {
     withPluginApi((api) => {
       const user = api.getCurrentUser();
+      const aiBotinHeader = api.container.lookup(
+        "service:site-settings"
+      )?.ai_bot_add_to_header;
 
-      if (user) {
+      // we can get stuck in a circular reference
+      // if the bot header button is enabled
+      // due to its fallback for lastKnownAppURL
+      if (!aiBotinHeader && user) {
         setDefaultHomepage("discourse-ai/ai-bot/conversations");
       }
     });
