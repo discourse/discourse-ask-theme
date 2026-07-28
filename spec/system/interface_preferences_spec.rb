e95b4b7ed79b2845c2f07d976f53695a40df19e0
Keegan George <kgeorge13@gmail.com>
2026-07-28 14:46:10 -0700

    UX: Show the `send_shortcut` preference on the interface preferences page

---
+++ spec/system/interface_preferences_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe "Interface preferences", type: :system do
+  fab!(:current_user, :user)
+
+  let(:interface_preferences) { PageObjects::Pages::UserPreferencesInterface.new }
+
+  before do
+    upload_theme_or_component
+    sign_in(current_user)
+  end
+
+  it "shows the send shortcut preference while hiding the rest of the other settings group" do
+    interface_preferences.visit(current_user)
+
+    expect(page).to have_css(".control-group.other .pref-send-shortcut")
+    expect(page).to have_no_css(".control-group.other .pref-enable-quoting")
+    expect(page).to have_no_css(".control-group.home")
+  end
+end
